8eed31fad04ffb43096e4752d41a5d7b6b3af4b7
Oliver Hutchinson <git@rainrental.org>
2025-08-31 21:22:22 +0100

    Bump version to 1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.rainrental.rainrentalrfid"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10009
-        versionName = "1.0.9"
+        versionCode = 10010
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
