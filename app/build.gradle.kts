8957d09499b23088c457dab926398b500e25e71a
Oliver Hutchinson <git@rainrental.org>
2025-09-10 11:22:02 +0100

    v1.0.30-prerelease: Prepare branch for pre-release bug fixes
    - Incremented version code to 10030
    - Set version name to 1.0.30-prerelease
    - Created prerelease-bug-fixes branch for upcoming fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.rainrental.rainrentalrfid"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10029
-        versionName = "1.0.29"
+        versionCode = 10030
+        versionName = "1.0.30-prerelease"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
