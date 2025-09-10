e408f5ba33b53a587288ee40eff88e7ae0909115
Oliver Hutchinson <git@rainrental.org>
2025-09-10 13:06:00 +0100

    Increment version to 1.0.33 for logging fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.rainrental.rainrentalrfid"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10032
-        versionName = "1.0.32-prerelease"
+        versionCode = 10033
+        versionName = "1.0.33"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
