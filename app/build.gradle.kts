d71ee3b1d7cfd3f5f25e0938f986b92034191316
Oliver Hutchinson <git@rainrental.org>
2025-09-11 14:08:29 +0100

    Increment version to 1.0.35 for UI layout improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.rainrental.rainrentalrfid"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10034
-        versionName = "1.0.34"
+        versionCode = 10035
+        versionName = "1.0.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
