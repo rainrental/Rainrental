1a748433616141a45b86fa822930a3a6f2277e18
Oliver Hutchinson <git@rainrental.org>
2025-09-17 01:57:18 +0100

    Increment version to 1.0.45 (10045)
    - Fix versionCode parsing issue in update check
    - App now correctly detects newer versions from API
    - Resolves 'No updates available' bug when updates exist

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.rainrental.rainrentalrfid"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10040
-        versionName = "1.0.40"
+        versionCode = 10045
+        versionName = "1.0.45"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
